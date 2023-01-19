c8b8a72526f602618b45be5f3cae5d79f0c79d3a
Dinu Neculai <nicu.dinu.neculai@gmail.com>
2023-01-19 11:31:12 +0200

    Creating the Main class, Game class, adding the first 2 packages.

---
+++ src/com/dinu/Main2048/Main.java
@@ -0,0 +1,25 @@
+package com.dinu.Main2048;
+
+import java.awt.*;
+
+public class Main extends Canvas implements Runnable {
+
+    public void start(){
+
+    }
+    public void stop(){
+
+    }
+    public void run(){
+
+    }
+    public void update(){
+
+    }
+    public void render(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/dinu/Main2048/game/Game.java
@@ -0,0 +1,16 @@
+package com.dinu.Main2048.game;
+
+public class Game {
+    public Game(){
+        init();
+    }
+    public void init(){
+
+    }
+    public void update(){
+
+    }
+    public void render(){
+
+    }
+}
